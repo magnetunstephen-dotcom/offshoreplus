dee74fcb428cf0479984c8897a3485919fd03c77
magnetunstephen-dotcom <magnetun.stephen@gmail.com>
2026-08-07 08:50:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   // Repository path while using magnetunstephen-dotcom.github.io/offshoreplus/.
   // Change to "/" when offshoreplus.no is connected.
-  base: "/offshoreplus/",
+  base: "/",
 });
